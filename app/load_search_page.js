cbfb2cf6d30040e08abe53daa14c75f336ebff32
pcbmaster <fitnessnonsense@gmail.com>
2018-12-22 23:14:29 -0700

    working puppeteer script

--- app/load_search_page.js
+++ app/load_search_page.js
@@ -1,3 +1,19 @@
+var puppeteer = require('puppeteer');
+var fs = require('fs');
+
+(async () => {
+    console.log("starting...");
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    console.log("going to KSL...");
+    await page.goto('https://www.ksl.com/auto/search/index?make[]=BMW&priceTo=7000&zip=84060&miles=100&transmission[]=Manual&drive[]=RWD&page=0&sort=0&perPage=96');
+    let bodyHTML = await page.evaluate(() =>  document.body.innerHTML);
+    console.log("writing to file...");
+    fs.writeFile('assets/search_page.html', bodyHTML, _ => console.log("done!"));
+    await browser.close();
+})();
+
+/*
 var page = new WebPage();
 var fs = require('fs');
 
@@ -11,3 +27,4 @@ page.open("https://www.ksl.com/auto/search/index?make[]=BMW&priceTo=7000&zip=840
     page.evaluate(function() {
     });
 });
+*/
--- app/scraper.rb
+++ app/scraper.rb
@@ -1,12 +1,15 @@
-require 'mechanize'
+require 'nokogiri'
 require 'rails/configuration'
 require 'pry'
 
 @ksl_url = 'https://www.ksl.com/auto/search/index?make[]=BMW&priceTo=7000&zip=84060&miles=100&transmission[]=Manual&drive[]=RWD&page=0&sort=2&perPage=96'
 @vin_decoder_url = 'https://www.mdecoder.com/decode/'
 
-@agent = Mechanize.new
-bmw_query = @agent.get(@ksl_url)
+#load our page and save it
+system("node ./load_search_page.js")
+html = File.read('assets/search_page.html')
+bmw_query = Nokogiri::HTML.parse(html)
+#bmw_query = @agent.get(@ksl_url) fix
 puts bmw_query.body
 listings = bmw_query.search('//div[contains(@class, "listing")]')
 listings.each do |listing|
